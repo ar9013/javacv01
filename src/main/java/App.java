01fef8bad8a561308557f47cc31e8015a78318ec
ar9013 <ar9013@gmail.com>
2017-04-16 10:12:04 +0800

    Hello JavaCV

---
+++ src/main/java/App.java
@@ -0,0 +1,26 @@
+import org.bytedeco.javacpp.opencv_core;
+
+import static org.bytedeco.javacpp.opencv_highgui.*;
+import static org.bytedeco.javacpp.opencv_imgcodecs.cvLoadImage;
+import static org.bytedeco.javacpp.opencv_imgcodecs.imread;
+
+/**
+ * Created by ar9013 on 4/16/17.
+ */
+public class App {
+
+    public static void main(String args[]) {
+        System.out.println("Hello World!");
+
+        opencv_core.IplImage src = cvLoadImage("Ariel_Lee.png");
+
+        cvNamedWindow("Display Image");
+
+        cvShowImage("Display Image", src); // 顯示影像於視窗
+
+        waitKey(0);
+
+        cvDestroyWindow("Display Image");
+
+    }
+}
